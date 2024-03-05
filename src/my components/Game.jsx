0c8a6c664be5f838f0c00e1756d7cb453a4e0c32
Daksh Maheshwari <dakshmaheshwari193@gmail.com>
2024-03-05 10:38:10 +0530

    Fix :{Home and Game section css fixed}

--- src/my components/Game.jsx
+++ src/my components/Game.jsx
@@ -103,10 +103,10 @@ export const Game = () => {
                {showRules? <div className="rulesCont">
                   <h4 id='howtoplay'>How to play dice game ?</h4>
                   <div className="rulesSec">
-                    <p className='ruleslist'>Select any number</p>
-                    <p className='ruleslist'>Click on dice image</p>
-                    <p className='ruleslist'>after click on  dice  if selected number is equal to dice number you will get same point as dice </p>
-                    <p className='ruleslist'>if you get wrong guess then  2 point will be dedcuted </p>
+                    <p className='ruleslist'>-Select any number</p>
+                    <p className='ruleslist'>-Click on dice image</p>
+                    <p className='ruleslist'>-after click on  dice  if selected number is equal to dice number you will get same point as dice </p>
+                    <p className='ruleslist'>-if you get wrong guess then  2 point will be dedcuted </p>
                   </div>
                </div> : ""}
 
